20b8e8e127faf2298ba30e6890b7a4b91c5db617
TaurusXin <taurusxin@outlook.com>
2022-01-24 17:31:42 +0000

    feat: direct running support

--- server/app.py
+++ server/app.py
@@ -15,19 +15,21 @@ app.config.from_object(config)
 # add custom logger handler
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
-handler = logging.FileHandler('./logs/temp.log')
-# datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.log'
+handler = logging.FileHandler('./logs/temp.log')  # datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.log'
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # initialize database
-logger.info('initializing database')
 db.init_app(app)
-logger.info('database initialized')
 
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!</h1>', 200
+
+
+if __name__ == '__main__':
+    logger.info('Start back-end at http://localhost:5000/...')
+    app.run(host='localhost', port=5000)
